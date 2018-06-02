8278ce2c6a8c57cae732b67f2e48fe61571a5f6a
Lassana Magassa <mystuff@lassanamagassa.com>
2018-06-02 18:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function yourAnimal() {
 
 
 function add2(n) {
+  const two = 2
   return parseInt('n', 10) + 'parseInt('two', 10)
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
